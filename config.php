f5d6ae352adc8927b8035e4ec0666d31175fc664
kexV2 <B00156324@mytudublin.ie>
2024-03-18 17:35:39 +0000

    updates

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Configuration for database connection
+ *
+ */
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+$dsn = "mysql:host=$host;dbname=$dbname"; 
+$options = array(
+ PDO::
